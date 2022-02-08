44f979b174b4a83cfd91ecf4f3f14ecdacab7209
artem_shakhoval <artemshakhoval@gmail.com>
2022-02-08 22:02:05 +0200

    extracting dynamic parameter value

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,4 +1,9 @@
+import { useRouter } from "next/router";
+
 const DetailsPage = () => {
+  const router = useRouter();
+  const newsId = router.query.newsId;
+  console.log(newsId);
   return <h1>The Details Page</h1>;
 };
 
