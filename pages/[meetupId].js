318174d116ac9bc44d7e3933b0c24cd56d308290
artem_shakhoval <artemshakhoval@gmail.com>
2022-02-09 18:21:27 +0200

    get pre-render fetch data using getStaticProps func

--- pages/[meetupId].js
+++ pages/[meetupId].js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const MeetupId = () => {
-  return <div>meetupId</div>;
+  return (
+    <section className="detail">
+      <img
+        src="https://img.theculturetrip.com/768x/smart/wp-content/uploads/2019/09/c267j3-2.jpg"
+        alt="A first meetup"
+      />
+      <h1>A First Meetup</h1>
+      <address>Barcelona, 24055, Some street</address>
+      <p>A first meetup</p>
+    </section>
+  );
 };
 
 export default MeetupId;
