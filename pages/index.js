0d843b7804020e8143a4eb82b3fec28180553035
artem_shakhoval <artemshakhoval@gmail.com>
2022-02-09 16:57:51 +0200

    add form for create new meetup and navigation

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,40 @@
 import React from "react";
+import MeetupList from "../components/meetups/MeetupList";
+import Layout from "../components/layout/Layout";
 
-const MeetupList = () => {
-  return <div>MeetupList</div>;
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://img.theculturetrip.com/768x/smart/wp-content/uploads/2019/09/c267j3-2.jpg",
+    address: "Barcelona, 24055, Some street",
+    description: "A First Meetup place",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://img.theculturetrip.com/768x/smart/wp-content/uploads/2015/12/15040570004_d78f0f11d2_z.jpg",
+    address: "Barcelona, 24055, Some street",
+    description: "A Second Meetup place",
+  },
+  {
+    id: "m3",
+    title: "A Third Meetup",
+    image:
+      "https://img.theculturetrip.com/768x/smart/wp-content/uploads/2019/09/c267j3-2.jpg",
+    address: "Barcelona, 24055, Some street",
+    description: "A Third Meetup place",
+  },
+];
+
+const HomePage = () => {
+  return (
+    <Layout>
+      <MeetupList meetups={DUMMY_MEETUPS} />
+    </Layout>
+  );
 };
 
-export default MeetupList;
+export default HomePage;
--- pages/new-meetup.js
+++ pages/new-meetup.js
@@ -1,7 +1,12 @@
 import React from "react";
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 const NewMeetup = () => {
-  return <div>NewMeetup</div>;
+  const addNewMeetupHandler = (enteredMeetup) => {
+    console.log(enteredMeetup);
+  };
+
+  return <NewMeetupForm onAddMeetup={addNewMeetupHandler} />;
 };
 
 export default NewMeetup;
